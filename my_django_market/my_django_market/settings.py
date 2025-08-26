840ad85760638c19e969dc3d84c714c1d44a66ef
Yevhenii Baibikov <jeka.spets333@gmail.com>
2025-08-26 08:39:45 +0200

    added static folder and connected it to settings

--- my_django_market/my_django_market/settings.py
+++ my_django_market/my_django_market/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/my_django_market/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
