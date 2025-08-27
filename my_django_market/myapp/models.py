41ee78b4eb1d9fd2a9bc4d87b165dd4d02154455
Yevhenii Baibikov <jeka.spets333@gmail.com>
2025-08-27 07:52:35 +0200

    added Item model

--- my_django_market/myapp/models.py
+++ my_django_market/myapp/models.py
@@ -8,6 +8,12 @@ class MyUser(AbstractUser):
         default=0.00
     )
 
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.CharField(max_length=100, null=False, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=0)
+
 
 
 
