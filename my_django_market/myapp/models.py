d5b09f5a5f4012d75e62602b372e9e64044d0c76
Yevhenii Baibikov <jeka.spets333@gmail.com>
2025-08-27 07:42:47 +0200

    added custom user model MyUser

--- my_django_market/myapp/models.py
+++ my_django_market/myapp/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
+
+class MyUser(AbstractUser):
+    balance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2, 
+        default=0.00
+    )
+
 
 
 
