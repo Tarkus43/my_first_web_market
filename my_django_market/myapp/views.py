9acbda6e01216fb342b9818313fe754707d895d8
Yevhenii Baibikov <jeka.spets333@gmail.com>
2025-09-03 16:58:10 +0200

    redirected main page to render main.html

--- my_django_market/myapp/views.py
+++ my_django_market/myapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from django.contrib.auth.views import LoginView
+from .models import Item
 
-class MainPageView(TemplateView):
-    paginate_by = 10
-    template_name = 'base.html'
+class MainPageView(ListView):
+    model = Item
+    paginate_by = 5
+    template_name = 'main.html'
 
 class Login(LoginView):
     success_url ='/'
